5dec2c8d3e744ce2034f2fb029efdac4017d939b
Nakakari <sarinandhika@gmail.com>
2023-03-09 17:31:25 +0700

    invoice 5

--- resources/views/Admin/Invoice/v_scriptReselect.blade.php
+++ resources/views/Admin/Invoice/v_scriptReselect.blade.php
@@ -164,16 +164,6 @@
             'style': 'multi'
         },
         "initComplete": function(row, data, dataIndex) {
-            const findDuplicates = (arr) => {
-                let sorted_arr = arr.slice().sort();
-                let results = [];
-                for (let i = 0; i < sorted_arr.length - 1; i++) {
-                    if (sorted_arr[i + 1] == sorted_arr[i]) {
-                        results.push(sorted_arr[i]);
-                    }
-                }
-                return results;
-            }
             let selected = {{ json_encode($selected) }}
             let arr = this.api().data();
             let dataku = []
